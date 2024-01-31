0a7495c002057212312861f51ccfb2f4d2e61f1e
qawafsec <mkd@weblock.su>
2024-02-01 01:45:12 +0300

    add getMessage () method

--- src/main/java/guru/qa/qa_guru_18_git/HelloController.java
+++ src/main/java/guru/qa/qa_guru_18_git/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Second Application!");
     }
+
+
+    private static String getMessage () {
+        return "Hello qa.guru";
+    }
 }
