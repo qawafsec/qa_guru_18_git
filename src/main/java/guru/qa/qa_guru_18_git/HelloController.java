fc3785978599b64186161c4a0b0732f0eaa9d6b6
qawafsec <mkd@weblock.su>
2024-02-05 20:58:29 +0300

    Change return Hello qa.guru, how r u

--- src/main/java/guru/qa/qa_guru_18_git/HelloController.java
+++ src/main/java/guru/qa/qa_guru_18_git/HelloController.java
@@ -6,18 +6,18 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-    private static String msg ="Hello qa.guru!";
+
 
     @FXML
     protected void onHelloButtonClick() {
-        System.out.println(msg);
+       
 
         welcomeText.setText("Welcome to JavaFX Second Application!123");
     }
 
 
     private static String getMessage () {
-        return "Hello qa.guru";
+        return "Hello qa.guru, how r u?";
 
     }
 }
