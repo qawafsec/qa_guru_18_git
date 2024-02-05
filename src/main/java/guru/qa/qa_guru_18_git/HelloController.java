603a1595a5804ce5b870bee9671cbae037a05e7d
qawafsec <154828886+qawafsec@users.noreply.github.com>
2024-02-05 20:57:30 +0300

    Update HelloController.java

--- src/main/java/guru/qa/qa_guru_18_git/HelloController.java
+++ src/main/java/guru/qa/qa_guru_18_git/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-    private static String msg ="Hello qa.guru!";
+    private static String msg ="Hello qa.guru!!!";
 
     @FXML
     protected void onHelloButtonClick() {
