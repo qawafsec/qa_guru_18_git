5c5a08192eba8a419213ec4e79c4a8028052f936
qawafsec <154828886+qawafsec@users.noreply.github.com>
2024-02-05 20:47:22 +0300

    Update HelloController.java

--- src/main/java/guru/qa/qa_guru_18_git/HelloController.java
+++ src/main/java/guru/qa/qa_guru_18_git/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController {
 
 
     private static String getMessage () {
-        return "Hello qa.guru!!!!!!";
+        return "Hello qa.guru!!!!!! Ok, let's go!";
 
     }
 }
